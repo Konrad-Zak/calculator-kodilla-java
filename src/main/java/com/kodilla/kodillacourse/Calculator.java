deb248c3e79aadd22783450292dbc3734bce8921
Ready2112 <kzak.air@gmail.com>
2020-02-03 19:39:04 +0100

    first commit

---
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.kodillacourse;
+
+public class Calculator {
+
+    public double add(double x, double y){
+        return x+y;
+    }
+
+    public double subtraction(double x, double y){
+        return x-y;
+    }
+}
