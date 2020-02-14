75a7234a39c7b2b56b01b2f062495460794edc27
macbookpro <kzak.air@gmail.com>
2020-02-14 16:30:33 +0100

    new package calculator with new class and one test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
@@ -13,5 +14,16 @@ public class TestingMain {
         } else {
             System.out.println("Error!");
         }
+
+        Calculator calculate = new Calculator();
+
+        Integer score = 10;
+
+        if (score.equals(calculate.add(5,5))){
+            System.out.println("add method test OK");
+        } else {
+            System.out.println("Error");
+        }
+
     }
 }
