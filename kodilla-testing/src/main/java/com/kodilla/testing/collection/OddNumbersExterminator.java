b64483cca1bad11bddd33adaf4727a54c21625ce
unknown <kzak.air@gmail.com>
2020-02-15 22:45:03 +0100

    work process in homework_6_3

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+
+public class OddNumbersExterminator {
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers){
+
+        return numbers;
+    }
+}
