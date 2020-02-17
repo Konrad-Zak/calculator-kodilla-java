54c26cb22c22fafcfb50df656dfee549b0583bf8
unknown <kzak.air@gmail.com>
2020-02-17 13:05:28 +0100

    in working

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
@@ -16,8 +16,39 @@ public class ForumUser {
     public void addPost(String author, String postBody){
         // do nothing
     }
-    public void addComment(ForumPost thePost, String author, String comment){
+
+    public void addComment(ForumPost thePost, String author, String commentBody){
         // do nothing
+    }
+
+    public int getPostsQuantity(){
+        // return 100 temporarily
+        return 100;
+    }
+
+    public int getCommentsQuantity(){
+        // return 100 temporarily
+        return 100;
+    }
+
+    public ForumPost getPost(int postNumber){
+        // returning null means that the operation was unsuccessful
+        return null;
+    }
+
+    public ForumComment getComment(int commentNumber){
+        // returning null means that the operation was unsuccessful
+        return null;
+    }
+
+    public boolean removePost(ForumPost thePost){
+        // return true temporarily
+        return true;
+    }
+
+    public boolean removeComment(ForumComment theComment){
+        // return true temporarily
+        return true;
     }
 
     public String getName() {
