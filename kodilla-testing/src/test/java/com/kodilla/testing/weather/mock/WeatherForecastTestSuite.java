c07a21075faaf01aac79eed0580526a79f912a4f
unknown <kzak.air@gmail.com>
2020-02-20 12:31:11 +0100

    in progress

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing.weather.mock;
+
+import com.kodilla.testing.weather.stub.Temperatures;
+import com.kodilla.testing.weather.stub.WeatherForecast;
+import org.junit.Assert;
+import static org.
+
+public class WeatherForecastTestSuite {
+}
