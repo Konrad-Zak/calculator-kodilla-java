4fff5d6398641b80d2543b5f20ba91a18d64ac3f
unknown <kzak.air@gmail.com>
2020-02-22 03:37:45 +0100

    new idea

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsCalculateTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsCalculateTestSuite.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.*;
 
@@ -11,59 +13,34 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class StatisticsCalculateTestSuite {
-    //test input data
-    String[] users =  new String[100];
-    List<String>testQuantityOfUser = Arrays.asList(users);
-    List<Integer> numberOfPosts = Arrays.asList(0,1000,100,100,100);
-    List<Integer> numberOfComments = Arrays.asList(100,100,1000,0,100);
 
-    //test result data
-    double[] resultAvgPostsPerUser =  {0,10,1,1,0};
-    double[] resultAvgCommentsPerUser = {1,1,10,0,0};
-    double[] resultAvgCommentsPerPost = {0,0.1,10,0,1};
-
-
-    //Variable
-    Statistics statisticsMock = mock(Statistics.class);
-    StatisticsCalculate statisticsCalculate = new StatisticsCalculate();
-
-    private static int testCounter = 0;
-    private int testQuantityOfPost;
-    private int testQuantityOfComment;
-
-    @BeforeEach
-    public void init(){
-        if (testCounter>3){
-            testQuantityOfUser = new ArrayList<>();
-        }
-        testQuantityOfPost = numberOfPosts.get(testCounter);
-        testQuantityOfComment = numberOfComments.get(testCounter);
-        testCounter ++;
-    }
-
-    @AfterEach
-    public void showStatistics(){
-        System.out.println(statisticsCalculate);
-    }
-
-    @RepeatedTest(value = 5)
-    public void calculateAdvStatistics () {
+    @ParameterizedTest
+    @CsvSource({"100,100,100,1,1,1", "10,100,100,10,10,1"})
+    public void testCalculateAdvStatistics(int quantityOfUsers, int quantityOfPosts, int quantityOfComments, int avgPostsPerUser, int avgCommentsPerUser, int avgCommentsPerPost ){
         //Given
-        when(statisticsMock.usersNames()).thenReturn(testQuantityOfUser);
-        when(statisticsMock.postsCount()).thenReturn(testQuantityOfPost);
-        when(statisticsMock.commentsCount()).thenReturn(testQuantityOfComment);
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.usersNames()).thenReturn(userGenerator(quantityOfUsers));
+        when(statisticsMock.postsCount()).thenReturn(quantityOfPosts);
+        when(statisticsMock.commentsCount()).thenReturn(quantityOfComments);
+        StatisticsCalculate statisticsCalculate = new StatisticsCalculate();
         //When
         statisticsCalculate.calculateAdvStatistics(statisticsMock);
         //Then
-        Assert.assertTrue(isStatisticsCalculate(statisticsCalculate));
+        Assert.assertTrue(ifCalculateAdvStatisticsEqual(statisticsCalculate,quantityOfUsers,quantityOfPosts,));
     }
-
-    boolean isStatisticsCalculate( StatisticsCalculate statisticsCalculate) {
-        return statisticsCalculate.getAvgPostsPerUser() == resultAvgPostsPerUser[testCounter-1] &&
-               statisticsCalculate.getAvgCommentsPerUser() == resultAvgCommentsPerUser[testCounter-1] &&
-               statisticsCalculate.getAvgCommentsPerPost() == resultAvgCommentsPerPost[testCounter-1] &&
-               statisticsCalculate.getQuantityOfUser() == (double) testQuantityOfUser.size() &&
-               statisticsCalculate.getQuantityOfPost() == (double) testQuantityOfPost &&
-               statisticsCalculate.getQuantityOfComment() == (double) testQuantityOfComment;
+    List<String> userGenerator(int quantity){
+        List<String> userList = new ArrayList<>();
+        for (int i = 0; i <quantity ; i++) {
+            userList.add("");
+        }
+        return userList;
+    }
+    boolean ifCalculateAdvStatisticsEqual(StatisticsCalculate statisticsCalculate,int quantityOfUsers, int quantityOfPosts, int quantityOfComments, int avgPostsPerUser, int avgCommentsPerUser, int avgCommentsPerPost ){
+        return  statisticsCalculate.getQuantityOfUser() == quantityOfUsers &&
+                statisticsCalculate.getQuantityOfPost() == quantityOfPosts &&
+                statisticsCalculate.getQuantityOfComment() == quantityOfComments &&
+                statisticsCalculate.getAvgPostsPerUser() == avgPostsPerUser &&
+                statisticsCalculate.getAvgCommentsPerUser() == avgCommentsPerUser &&
+                statisticsCalculate.getAvgCommentsPerPost() == avgCommentsPerPost;
     }
 }
