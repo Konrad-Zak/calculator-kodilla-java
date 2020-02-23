fc002e052e65f6a3768042dddbf5c4c37ce06592
unknown <kzak.air@gmail.com>
2020-02-23 02:29:34 +0100

    sub module 7.2 example, new package immutable with classes( Book, ForumUser ) and iterate

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -13,5 +14,8 @@ public class StreamMain {
         poemBeautifier.beautify("asdfghj",String::toUpperCase);
         poemBeautifier.beautify("NOWA_HUTA",String::toLowerCase);
         poemBeautifier.beautify("CALL TO ",text -> text.concat(addText));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
