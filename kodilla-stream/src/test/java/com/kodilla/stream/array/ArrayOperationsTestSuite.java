bf9aca840edb3fbe452f3e992c9d7580349c1629
unknown <kzak.air@gmail.com>
2020-02-26 19:26:27 +0100

    after

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -3,13 +3,36 @@ package com.kodilla.stream.array;
 import org.junit.Assert;
 import org.junit.Test;
 
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+
+@RunWith(Parameterized.class)
 public class ArrayOperationsTestSuite {
+
+    private  int[] numbers;
+    private double avgExpected;
+
+    public ArrayOperationsTestSuite(int[] numbers, double avgExpected) {
+        this.numbers = numbers;
+        this.avgExpected = avgExpected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { new int[]{1,2,3,4,5,6}, 3.5 },
+                { new int[]{2,3,2,3}, 2.5 },
+                { new int[]{5,5,5,5,5}, 5.0 }
+        });
+    }
 
     @Test
     public void testGetAverage() {
-        //Given
-        int[] numbers = {1,2,3,4,5,6};
-        double avgExpected = 3.5;
         //When
         double avgResult = ArrayOperations.getAverage(numbers);
         //Then
