5fdabf504943168f4f94665ec8087db2312e1fc9
unknown <kzak.air@gmail.com>
2020-03-05 19:53:22 +0100

    after use Builder

--- kodilla-rps/src/main/java/com/kodilla/rps/movement/MovementKey.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/movement/MovementKey.java
@@ -4,18 +4,53 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MovementKey {
-    private final String moveName;
-    private final String beatOne;
-    private final String beatTwo;
-    private final String loseOne;
-    private final String loseTwo;
+    private String moveName;
+    private String beatOne;
+    private String beatTwo;
+    private String loseOne;
+    private String loseTwo;
 
-    public MovementKey(String moveName, String beatOne, String beatTwo, String loseOne, String loseTwo) {
-        this.moveName = moveName;
-        this.beatOne = beatOne;
-        this.beatTwo = beatTwo;
-        this.loseOne = loseOne;
-        this.loseTwo = loseTwo;
+    public static final class Builder{
+        private String moveName;
+        private String beatOne;
+        private String beatTwo;
+        private String loseOne;
+        private String loseTwo;
+
+        public Builder moveName(String moveName){
+            this.moveName = moveName;
+            return this;
+        }
+
+        public Builder beatOne(String beatOne){
+            this.beatOne = beatOne;
+            return this;
+        }
+
+        public Builder beatTwo(String beatTwo){
+            this.beatTwo = beatTwo;
+            return this;
+        }
+
+        public Builder loseOne(String loseOne){
+            this.loseOne = loseOne;
+            return this;
+        }
+
+        public Builder loseTwo(String loseTwo){
+            this.loseTwo = loseTwo;
+            return this;
+        }
+
+        public MovementKey build(){
+            MovementKey movementKey = new MovementKey();
+            movementKey.moveName = this.moveName;
+            movementKey.beatOne = this.beatOne;
+            movementKey.beatTwo = this.beatTwo;
+            movementKey.loseOne = this.loseOne;
+            movementKey.loseTwo = this.loseTwo;
+            return movementKey;
+        }
     }
 
     public String getMoveName() {
