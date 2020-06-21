ab1399d1608b0e65779be1da5570081d7024d01e
Ready2112 <kzak.air@gmail.com>
2020-06-21 18:37:57 +0200

    small correct

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/aop/WatcherOrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/aop/WatcherOrderFacade.java
@@ -1,8 +1,11 @@
 package com.kodilla.patterns2.facade.api.aop;
 
+import com.kodilla.patterns2.facade.Item;
 import com.kodilla.patterns2.facade.Order;
+import com.kodilla.patterns2.facade.api.OrderDto;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,10 +16,11 @@ public class WatcherOrderFacade {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WatcherOrderFacade.class);
 
-    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
-    public void logEvent(){
-        LOGGER.info("Login the event: used processOrder ");
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))"
+    + " && args(orderDto,idUser)" + " && target(object)")
+    public void logEvent(OrderDto orderDto, Long idUser, Object object){
+        LOGGER.info("Login the event... Class: " + object.getClass().getName() +
+                " Args: userId: " + idUser );
     }
-
 
 }
